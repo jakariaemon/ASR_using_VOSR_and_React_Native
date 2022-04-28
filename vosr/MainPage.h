ea234670e4e86a2bbdf2b385c0101cbad0093764
jakariaemon <jakariaszlzmm12@gmail.com>
2022-04-28 15:30:38 +0600

    Add project files.

---
+++ vosr/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::vosr::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::vosr::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
